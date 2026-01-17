37a9b869afa9a01936f6ef2589b7262210f14a90
adityakashyap5047 <adityakashyap5047@gmail.com>
2026-01-17 23:26:38 +0530

    enhancing the custom hook

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,4 +1,6 @@
-import { useState } from "react"
+"use client";
+
+import { useEffect, useState, useCallback } from "react";
 import { toast } from "sonner";
 
 type ApiResponse<T> = {
@@ -7,32 +9,50 @@ type ApiResponse<T> = {
     error?: string;
 };
 
-const useFetch = <T, Args extends unknown[] = unknown[]>(cb: (...args: Args) => Promise<ApiResponse<T>>) => {
+const useFetch = <T, Args extends unknown[] = unknown[]>(
+    cb: (...args: Args) => Promise<ApiResponse<T>>,
+    options: {
+        autoFetch?: boolean;
+        args?: Args;
+    } = {},
+) => {
     const [data, setData] = useState<T | undefined>(undefined);
-    const [loading, setLoading] = useState<boolean>(false);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
-    const fn = async(...args: Args) => {
-        setLoading(true);
-        setError(null);
+    const { autoFetch = true, args = [] as unknown as Args } = options;
 
-        try {
-            const response = await cb(...args);
-            if(response && response.success) {
-                setData(response.data);
-            } else {
-                setError(response.error || 'An error occurred');
-                toast.error(response.error || 'An error occurred');
+    const fn = useCallback(
+        async (...params: Args) => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await cb(...params);
+                if (response?.success) {
+                    setData(response.data);
+                } else {
+                    setError(response?.error || "An error occurred");
+                    toast.error(response?.error || "An error occurred");
+                }
+            } catch (err) {
+                const msg = err instanceof Error ? err.message : "An error occurred";
+                setError(msg);
+                toast.error(msg);
+            } finally {
+                setLoading(false);
             }
-        } catch (error) {
-            setError(error instanceof Error ? String(error.message) : "An error occurred");
-            toast.error(error instanceof Error ? String(error.message) : "An error occurred");
-        } finally {
-            setLoading(false);
-        }
-    }
+        },
+        [cb],
+    );
 
-    return {data, loading, error, fn, setData};
-}
+    useEffect(() => {
+        if (autoFetch) {
+            fn(...args);
+        }
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, [autoFetch]);
+
+    return { data, loading, error, fn, setData };
+};
 
 export default useFetch;
