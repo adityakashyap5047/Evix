ac6a2631f62c0ba1e41bdb7b6d3ad57fd5fb9c1a
adityakashyap5047 <adityakashyap5047@gmail.com>
2026-01-19 18:25:00 +0530

    created the upgrade modal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,46 @@
 "use client"
 
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from './ui/button'
 import { useState } from 'react'
-import { Building, Plus, Ticket } from 'lucide-react'
+import { Building, Crown, Plus, Ticket } from 'lucide-react'
 import { OnboardingModal } from './onboarding-modal'
 import { useOnboarding } from '@/hooks/use-onboarding'
+import SearchLocationBar from './search-location-bar'
+import { Badge } from './ui/badge'
+import UpgradeModal from './upgrade-modal'
 
 const Header = () => {
 
     const [showUpgradeModel, setShowUpgradeModel] = useState<boolean>(false)
     const { showOnboarding, handleOnboardingComplete, handleOnboardingSkip } = useOnboarding();
 
+    const { has } = useAuth();
+    const hasPro = has?.({plan: "pro"}) ?? false;
+
     return (
         <>
             <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b'>
-                <div className='max-w-5xl mx-auto px-6 py-4 flex items-center justify-between'>
-                    <Link href={"/"}>
+                <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
+                    <Link href={"/"} className='flex items-center'>
                         <Image src={"/Evix.png"} alt="Evix Logo" width={100} height={100} className='w-12 h-12' priority />
+                    
+                        {hasPro && (
+                            <Badge className='bg-linear-to-r from-pink-500 to-orange-500 gap-1 text-white ml-3'>
+                                <Crown className='w-3 h-3' />
+                                Pro 
+                            </Badge>
+                        )}
                     </Link>
 
+                    <div className="hidden md:flex flex-1 justify-center">
+                        <SearchLocationBar />
+                    </div>
+
                     <div className="flex items-center">
-                        <Button variant={"ghost"} size={"sm"} onClick={() => setShowUpgradeModel(true)}>Pricing</Button>
+                       {!hasPro && <Button variant={"ghost"} size={"sm"} onClick={() => setShowUpgradeModel(true)}>Pricing</Button>}
 
                         <Button variant={"ghost"} size={"sm"} asChild className='mr-2'>
                             <Link href={"/explore"}>Explore</Link>
@@ -58,11 +75,22 @@ const Header = () => {
                         </SignedOut>
                     </div>
                 </div>
+
+                <div className="md:hidden border-t px-3 py-3">
+                    <SearchLocationBar />
+                </div>
+
             </nav>
             <OnboardingModal
                 isOpen={showOnboarding}
                 onClose={handleOnboardingSkip}
                 onComplete={handleOnboardingComplete}
+            />
+
+            <UpgradeModal
+                isOpen={showUpgradeModel}
+                onClose={() => setShowUpgradeModel(false)}
+                trigger="header"
             />
         </>
     )
